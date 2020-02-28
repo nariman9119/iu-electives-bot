8f520b546cb1d4b2279c4cabba9180ac8d7461ee
nariman9119 <nariman9119@gmail.com>
2020-02-29 00:03:44 +0300

    minor changes, TODO added

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 -- | This todo bot can add items, remove them
 -- and show a current list of things to do.
 module Main where
@@ -16,13 +17,17 @@ import           Telegram.Bot.Simple.UpdateParser
 
 -- | Bot conversation state model.
 data Model = Model
-  { electiveCourses :: [ElectiveCourse]
-  , myElectiveCourses :: [ElectiveCourse]
-  , currentTime :: UTCTime      -- ^ A list of todo items.
+  { electiveCourses :: [ElectiveCourse] --list of all elective courses
+  , myElectiveCourses :: [ElectiveCourse] --elective courses that user will choose
+  , currentTime :: UTCTime 
   } deriving (Show)
+
 
 data ElectiveCourse = ElectiveCourse { electiveCourseTitle :: Text 
                                      , electiveCourseReminder :: Maybe UTCTime
+                                     --, electiveCourseLecturer :: Text --TODO
+                                     --, electiveCourseRoomNumber :: Text
+                                     --, electiveCourseTime :: Text 
                              } deriving (Show)  
 
 initialModel :: IO Model
@@ -34,47 +39,33 @@ initialModel = do
                     , ElectiveCourse { electiveCourseTitle = "elective1" , electiveCourseReminder = Nothing}]
                     , myElectiveCourses = [],currentTime = now}
 
--- | Create a new todo item with just a title.
+
+-- Do not provide possibility to add course manually to the list of all courses
 mkCourse :: Text -> ElectiveCourse
 mkCourse title = ElectiveCourse { electiveCourseTitle = title, electiveCourseReminder = Nothing }
 
--- | Ade new course to elective course list.
+
+-- | Add course to user`s list from list of all courses.
 addCourse :: ElectiveCourse -> Model -> Model
 addCourse course model = model { myElectiveCourses = course : myElectiveCourses model }
 
--- | Remove an item from todo list
+-- | Ability to remove course from user`s list
 removeCourse :: Text -> Model -> Model
 removeCourse title model = model { myElectiveCourses = filter p (myElectiveCourses model) }
   where
     p item = electiveCourseTitle item /= title
 
--- | Pretty print a single todo item.
-ppCourse :: ElectiveCourse -> Text
-ppCourse course = "- " <> electiveCourseTitle course <> "\n"
-
--- | Pretty print a todo list.
-ppCourses :: Model -> Text
-ppCourses model =
-  case foldMap ppCourse (electiveCourses model) of
-    ""    -> "Would like to add an elective course? :)"
-    items -> "Elective courses selected:\n" <> items
-
--- | Pretty print a todo list.
-ppMyCourses :: Model -> Text
-ppMyCourses model =
-  case foldMap ppCourse (myElectiveCourses model) of
-    ""    -> "Would like to add an elective course? :)"
-    items -> "Elective courses selected:\n" <> items
+--TODO restrict possibility to add same course many times.
 
 -- | Actions bot can perform.
 data Action
   = NoAction                -- ^ Perform no action.
-  | SetTime UTCTime         -- ^ Update current time.
   | AddItem Text            -- ^ Add a new todo item.
   | RemoveItem Text         -- ^ Remove an item by its title.
   | ShowItems               -- ^ Display all items (either with a new message or by updating existing one).
   | ShowAllCourses
   | Start                   -- ^ Display start message.
+  | SetTime UTCTime         -- ^ Update current time.
   | RevealItemActions Text  -- ^ Update list of items to display item actions.
   | SetReminderIn Int Text  -- ^ Set a reminder for an item in a given amount of minutes from now.
   deriving (Show, Read)
@@ -95,12 +86,14 @@ initBot = do
       ]
     }
 
+
+--TODO reimplement reminder
 setReminderIn :: Int -> Text -> Model -> Model
 setReminderIn minutes title model = setReminder title alarmTime model
   where
     now = currentTime model
     alarmTime = addUTCTime (fromIntegral (60 * minutes)) now
-
+--TODO reimplement reminder
 -- | Set an absolute alarm time for an item with a given title.
 setReminder :: Text -> UTCTime -> Model -> Model
 setReminder title datetime model = model
@@ -109,7 +102,8 @@ setReminder title datetime model = model
       updateReminder item
         | title /= electiveCourseTitle item = item
         | otherwise = item { electiveCourseReminder = Just datetime }
-        
+
+--TODO reimplement reminder        
 courseReminder :: Model -> Eff Action Model
 courseReminder model = do
   newItems <- mapM electiveReminder (electiveCourses model)
@@ -124,22 +118,25 @@ courseReminder model = do
           return item { electiveCourseReminder = Nothing }
         _ -> return item
 
+
+
 startMessage :: Text
 startMessage = Text.unlines
- [ "Introduction text"
- , "Text about functionality of this bot"
+ [ "Welcome to Elective course schedule"
  ]
 
- -- | A start keyboard with some helpful todo suggestions.
+
+ -- | A start keyboard with commands to start with.
 startMessageKeyboard :: Telegram.ReplyKeyboardMarkup
 startMessageKeyboard = Telegram.ReplyKeyboardMarkup
   { Telegram.replyKeyboardMarkupKeyboard =
-      [ [ "Start", "Show" ]
+      [ [ "/start", "/show" ]
       ]
   , Telegram.replyKeyboardMarkupResizeKeyboard = Just True
   , Telegram.replyKeyboardMarkupOneTimeKeyboard = Just True
   , Telegram.replyKeyboardMarkupSelective = Just True
   }
+
 
 
 myCoursesAsInlineKeyboard :: Model -> EditMessage
@@ -150,6 +147,7 @@ myCoursesAsInlineKeyboard model =
       { editMessageReplyMarkup = Just $
           Telegram.SomeInlineKeyboardMarkup (myCoursesInlineKeyboard items)
       }
+
 myCoursesInlineKeyboard :: [ElectiveCourse] -> Telegram.InlineKeyboardMarkup
 myCoursesInlineKeyboard
   = Telegram.InlineKeyboardMarkup .  map (pure . myCourseInlineKeyboardButton)
@@ -160,36 +158,32 @@ myCourseInlineKeyboardButton item = actionButton title (RevealItemActions title)
     title = electiveCourseTitle item
 
 
-itemsAsInlineKeyboard :: Model -> EditMessage
-itemsAsInlineKeyboard model =
+coursesAsInlineKeyboard :: Model -> EditMessage
+coursesAsInlineKeyboard model =
   case electiveCourses model of
     [] -> "The list of elective courses is not yet available"
     items -> (toEditMessage "List of available elective courses")
       { editMessageReplyMarkup = Just $
-          Telegram.SomeInlineKeyboardMarkup (itemsInlineKeyboard items)
+          Telegram.SomeInlineKeyboardMarkup (coursesInlineKeyboard items)
       }
+coursesInlineKeyboard :: [ElectiveCourse] -> Telegram.InlineKeyboardMarkup
+coursesInlineKeyboard
+  = Telegram.InlineKeyboardMarkup .  map (pure . courseInlineKeyboardButton)
 
--- | Inline keyboard with every button representing one item.
-itemsInlineKeyboard :: [ElectiveCourse] -> Telegram.InlineKeyboardMarkup
-itemsInlineKeyboard
-  = Telegram.InlineKeyboardMarkup .  map (pure . itemInlineKeyboardButton)
-
--- | Inline keyboard button for a given todo item.
-itemInlineKeyboardButton :: ElectiveCourse -> Telegram.InlineKeyboardButton
-itemInlineKeyboardButton item = actionButton title (AddItem title)
+courseInlineKeyboardButton :: ElectiveCourse -> Telegram.InlineKeyboardButton
+courseInlineKeyboardButton item = actionButton title (AddItem title)
   where
     title = electiveCourseTitle item
 
 
     
-    -- | Actions to do with an item as an inline keyboard message.
-itemActionsMessage :: Text -> EditMessage
-itemActionsMessage title = (toEditMessage (Text.unlines ["«" <> title <> "»", "Course Instructor: Test Testovich", "Next Lecture: 30.02.2020 09:00"]))
+myCourseActionsMessage :: Text -> EditMessage
+myCourseActionsMessage title = (toEditMessage (Text.unlines ["«" <> title <> "»", "Course Instructor: Test Testovich", "Next Lecture: 30.02.2020 09:00"]))
   { editMessageReplyMarkup = Just $
-      Telegram.SomeInlineKeyboardMarkup (itemActionsKeyboard title) }
+      Telegram.SomeInlineKeyboardMarkup (myCourseActionsKeyboard title) }
 
-itemActionsKeyboard :: Text -> Telegram.InlineKeyboardMarkup
-itemActionsKeyboard title = Telegram.InlineKeyboardMarkup
+myCourseActionsKeyboard :: Text -> Telegram.InlineKeyboardMarkup
+myCourseActionsKeyboard title = Telegram.InlineKeyboardMarkup
   [ [ btnRemindIn ]
   , [ btnBack ]
   ]
@@ -207,10 +201,9 @@ itemActionsKeyboard title = Telegram.InlineKeyboardMarkup
 handleUpdate :: Model -> Telegram.Update -> Maybe Action
 handleUpdate _ = parseUpdate
     $ ShowItems   <$  command "show"
-  <|> RemoveItem  <$> (command "remove" <|> command "done")
+  <|> RemoveItem  <$> command "remove" 
   <|> Start       <$  command "start"
   <|> callbackQueryDataRead
-
   <|> AddItem     <$> text
 
 
@@ -219,20 +212,26 @@ handleUpdate _ = parseUpdate
 handleAction :: Action -> Model -> Eff Action Model
 handleAction action model = case action of
   NoAction -> pure model
+  -- TODO reimplement reminder
   SetTime t -> model { currentTime = t } <# do
     SetTime <$> liftIO (threadDelay 1000 >> getCurrentTime)
+  -- add course by creating new course from selected one
   AddItem title -> addCourse (mkCourse title) model <# do
-    replyText "Noted."
+    replyText "Course added to your list"
     pure NoAction
+  -- remove course from list of user`s courses
   RemoveItem title -> removeCourse title model <# do
-    replyText ("Removed item: " <> title)
+    replyText ("Course " <> title <> "removed from your list") 
     pure ShowItems
+  -- show list of your courses
   ShowItems -> model <# do
     replyOrEdit (myCoursesAsInlineKeyboard model)
     pure NoAction
+  -- show list of all courses
   ShowAllCourses -> model <# do
-    replyOrEdit (itemsAsInlineKeyboard model)
+    replyOrEdit (coursesAsInlineKeyboard model)
     pure NoAction
+  -- start telegram bot 
   Start -> do
     eff $ do
       reply (toReplyMessage startMessage)
@@ -242,14 +241,15 @@ handleAction action model = case action of
       pure ShowAllCourses
     eff $ SetTime <$> liftIO getCurrentTime
     pure model
+-- show actions from course that was selected on user`s list
   RevealItemActions title -> model <# do
-    editUpdateMessage (itemActionsMessage title)
+    editUpdateMessage (myCourseActionsMessage title)
     pure NoAction
+--TODO reimplement reminder
   SetReminderIn minutes title -> setReminderIn minutes title model <# do
     replyText "Ok, I will remind you."
     pure NoAction
 
--- | Run bot with a given 'Telegram.Token'.
 run :: Telegram.Token -> IO ()
 run token = do
   env <- Telegram.defaultTelegramClientEnv token
@@ -257,9 +257,6 @@ run token = do
   startBot_ (useLatestUpdateInJobs (traceBotDefault bot)) env
 
 
--- | Run bot using 'Telegram.Token' from @TELEGRAM_BOT_TOKEN@ environment.
--- main :: IO ()
--- main = getEnvToken "token" >>= run
 main :: IO ()
 main = do 
   putStrLn "Please, enter Telegram bot's API token:" 
